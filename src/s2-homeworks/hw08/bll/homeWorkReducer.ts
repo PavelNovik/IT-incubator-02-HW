7879eadc9a4ad1cdf3629ee7e7d1f5d339ea994f
Pavel Novik <brn2befree@gmail.com>
2023-11-23 23:20:24 +0300

    finish hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,15 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const compare = (a: UserType, b: UserType) => action.payload === 'up' ? a.name.charCodeAt(0) - b.name.charCodeAt(0) : b.name.charCodeAt(0) - a.name.charCodeAt(0)
+            return [...state].sort(compare)// need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state].sort((a,b)=> a.age - b.age).filter(u => u.age >= 18) // need to fix
         }
         default:
             return state
